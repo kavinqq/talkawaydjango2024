59c7f80093e07b0c672958c8347854523d79e427
KuanLin <kuanlin72043@gmail.com>
2024-06-09 18:07:31 +0800

    fix: 將serializer 跟 views改回來(之前有加入dall-e測試的部分)

--- gpt/serializers.py
+++ gpt/serializers.py
@@ -5,7 +5,3 @@ class ChatWithGPTSerializer(serializers.Serializer):
     session_id = serializers.CharField(help_text="會話ID", allow_null=False, allow_blank=False, required=False)
     scenario = serializers.CharField(help_text="情景", allow_null=False, allow_blank=False, required=False)
     input_text = serializers.CharField(help_text="給ChatGPT的內容", allow_null=False, allow_blank=False, required=False)
-
-
-class DellETestSerializer(serializers.Serializer):
-    input_text = serializers.CharField(help_text="給ChatGPT的內容", allow_null=False, allow_blank=False, required=False)
