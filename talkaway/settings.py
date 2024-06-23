3fe2cf5b43664dfd856d34267e0ea5102ee7a42d
KuanLin <kuanlin72043@gmail.com>
2024-06-23 22:06:53 +0800

    feat: CORS cross plugin and setting

--- talkaway/settings.py
+++ talkaway/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party apps
+    'corsheaders',
     'drf_spectacular_sidecar',
     'drf_spectacular',
     'rest_framework',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -215,3 +217,9 @@ SPECTACULAR_SETTINGS = {
 
 # OpneAI API Key
 openai.api_key = OPENAI_API_KEY
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
